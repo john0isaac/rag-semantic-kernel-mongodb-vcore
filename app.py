74aa32b5b9fdb5205ab160ce2296ef4458a541b3
John A <johnaziz269@gmail.com>
2024-02-13 14:51:02 +0200

    remove debugging mode

--- app.py
+++ app.py
@@ -19,5 +19,13 @@ async def get_response():
         rag_option = rag_or_vector
     )
 
+# Default port:
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+
+# Or specify port manually:
+"""
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+"""
