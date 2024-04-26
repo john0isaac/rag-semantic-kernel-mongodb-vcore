b9914f92dc4db533c2be763787638de354516f9d
John Aziz <johnaziz269@gmail.com>
2024-04-26 21:41:31 +0000

    define app as async function

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,18 +4,18 @@ import pytest_asyncio
 from quartapp import create_app
 
 
-@pytest.fixture
-def app():
+@pytest_asyncio.fixture
+async def app():
     app = create_app()
     app.config.update(
         {
             "TESTING": True,
         }
     )
+    async with app.test_app() as test_app:
+        yield test_app
 
-    return app.test_app()
 
-
-@pytest_asyncio.fixture
-async def client(app):
-    yield app.test_client()
+@pytest.fixture
+def client(app):
+    return app.test_client()
